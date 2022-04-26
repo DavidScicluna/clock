8a46c79e717aa36ad29830eb846fc9eb6049c949
David Scicluna <dave_nostra@hotmail.com>
2022-04-26 21:46:44 +0200

    Fix: TimeProgress - Extracted label logic

--- src/components/TimeProgress/index.tsx
+++ src/components/TimeProgress/index.tsx
@@ -1,11 +1,8 @@
-import { FC, useCallback } from 'react';
+import { FC } from 'react';
 
 import { useTheme, handleHue } from '@davidscicluna/component-library';
 
-import { ColorMode, useColorMode, useBoolean, CircularProgress, CircularProgressLabel, Text } from '@chakra-ui/react';
-
-import { compact } from 'lodash';
-import { useUpdateEffect } from 'usehooks-ts';
+import { ColorMode, useColorMode, CircularProgress, CircularProgressLabel, VStack } from '@chakra-ui/react';
 
 import { TimeProgressProps } from './types';
 
@@ -13,48 +10,9 @@ const TimeProgress: FC<TimeProgressProps> = (props) => {
 	const theme = useTheme();
 	const { colorMode: colorModeHook } = useColorMode();
 
-	const { color, colorMode: colorModeProp, timer, ...rest } = props;
-
-	const [hasHours, setHasHours] = useBoolean();
-	const [hasMinutes, setHasMinutes] = useBoolean();
+	const { children, color, colorMode: colorModeProp, ...rest } = props;
 
 	const colorMode: ColorMode = colorModeProp || colorModeHook;
-
-	const handleTimerLabel = useCallback((): string => {
-		const { hours = 0, minutes = 0, seconds = 0, milliseconds = 0 } = timer;
-
-		const hr = hours > 0 ? (hours >= 10 ? hours : `0${hours}`) : 0;
-		const min =
-			minutes > 0 || (hours > 0 && minutes === 0)
-				? minutes >= 10
-					? minutes
-					: minutes > 0
-					? `0${minutes}`
-					: '00'
-				: 0;
-		const sec = seconds >= 10 ? seconds : `0${seconds}`;
-		const ms = milliseconds >= 10 ? milliseconds : `0${milliseconds}`;
-
-		return compact([hr, min, `${sec}.${ms}`]).join(':');
-	}, [timer]);
-
-	const handleCheckTimer = useCallback(() => {
-		const { hours = 0, minutes = 0 } = timer;
-
-		if (hours > 0) {
-			setHasHours.on();
-		} else {
-			setHasHours.off();
-		}
-
-		if (minutes > 0 || (hours > 0 && minutes === 0)) {
-			setHasMinutes.on();
-		} else {
-			setHasMinutes.off();
-		}
-	}, [timer]);
-
-	useUpdateEffect(() => handleCheckTimer(), [timer]);
 
 	return (
 		<CircularProgress
@@ -70,15 +28,9 @@ const TimeProgress: FC<TimeProgressProps> = (props) => {
 			}}
 		>
 			<CircularProgressLabel width='75%'>
-				<Text
-					align='center'
-					color={`gray.${handleHue(colorMode, 'gray')}`}
-					fontSize={hasHours && hasMinutes ? '4xl' : hasMinutes ? '5xl' : '6xl'}
-					whiteSpace='nowrap'
-					userSelect='none'
-				>
-					{handleTimerLabel()}
-				</Text>
+				<VStack width='100%' spacing={0}>
+					{children}
+				</VStack>
 			</CircularProgressLabel>
 		</CircularProgress>
 	);
--- src/components/TimeProgress/types.ts
+++ src/components/TimeProgress/types.ts
@@ -1,15 +1,15 @@
+import { ReactNode } from 'react';
+
 import { Color } from '@davidscicluna/component-library';
 
 import { CircularProgressProps, ColorMode } from '@chakra-ui/react';
-
-import { Timer } from '../../common/types';
 
 export type TimeProgressColor = Exclude<Color, 'transparent'>;
 
 type Omitted = 'getValueText' | 'isIndeterminate' | 'size' | 'thickness' | 'trackColor';
 
 export type TimeProgressProps = {
+	children: ReactNode;
 	color: TimeProgressColor;
 	colorMode?: ColorMode;
-	timer: Timer;
 } & Omit<CircularProgressProps, Omitted>;
