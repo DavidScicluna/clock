637e045886b6c6785780d8885ad4015a9b292fa0
David Scicluna <dave_nostra@hotmail.com>
2022-04-29 21:10:04 +0200

    Feature: Stopwatch - Laps - Added new lap animation

--- src/pages/Stopwatch/components/Laps/index.tsx
+++ src/pages/Stopwatch/components/Laps/index.tsx
@@ -2,20 +2,37 @@ import { FC } from 'react';
 
 import { Divider } from '@davidscicluna/component-library';
 
-import { VStack } from '@chakra-ui/react';
+import { useBoolean, VStack, SlideFade } from '@chakra-ui/react';
+
+import { AnimatePresence } from 'framer-motion';
+import { useUpdateEffect } from 'usehooks-ts';
 
 import { LapsProps } from './types';
 import Lap from './components/Lap';
 
 const Laps: FC<LapsProps> = ({ laps = [], timer }) => {
-	return (
-		<VStack width='100%' height='100%' overflowY='auto' divider={<Divider />}>
-			<Lap {...timer} index={laps.length + 1} status='default' />
+	const [animate, setAnimate] = useBoolean(true);
 
-			{laps.map((lap, index) => (
-				<Lap {...lap} key={index} />
-			))}
-		</VStack>
+	useUpdateEffect(() => {
+		setAnimate.off();
+
+		setTimeout(() => setAnimate.on(), 100);
+	}, [laps]);
+
+	return (
+		<AnimatePresence>
+			<VStack width='100%' divider={<Divider />}>
+				{animate && (
+					<SlideFade key='active_lap' in unmountOnExit offsetY={9} style={{ width: '100%' }}>
+						<Lap {...timer} index={laps.length + 1} status='default' />
+					</SlideFade>
+				)}
+
+				{laps.map((lap, index) => (
+					<Lap {...lap} key={index} />
+				))}
+			</VStack>
+		</AnimatePresence>
 	);
 };
 
