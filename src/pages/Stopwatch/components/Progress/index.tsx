302e2c315da422b3d48a810995e8439578b675dc
David Scicluna <dave_nostra@hotmail.com>
2022-04-30 18:33:14 +0200

    Fix: Stopwatch - Progress - Added TimeLabel

--- src/pages/Stopwatch/components/Progress/index.tsx
+++ src/pages/Stopwatch/components/Progress/index.tsx
@@ -1,23 +1,16 @@
-import { FC, useState, useCallback, useEffect } from 'react';
+import { FC, useCallback, useEffect } from 'react';
 
-import { useTheme, getColor } from '@davidscicluna/component-library';
+import { useBoolean } from '@chakra-ui/react';
 
-import { useColorMode, useBoolean, Text } from '@chakra-ui/react';
-
+import TimeLabel from '../../../../components/TimeLabel';
 import TimeProgress from '../../../../components/TimeProgress';
 import { checkTimer } from '../../../../common/utils';
-import { getStopwatchLabel } from '../../common/utils';
 
 import { ProgressProps } from './types';
 
 const color = 'blue';
 
 const Progress: FC<ProgressProps> = ({ timer }) => {
-	const theme = useTheme();
-	const { colorMode } = useColorMode();
-
-	const [time, setTime] = useState<string>('');
-
 	const [hasHours, setHasHours] = useBoolean();
 	const [hasMinutes, setHasMinutes] = useBoolean();
 
@@ -35,24 +28,13 @@ const Progress: FC<ProgressProps> = ({ timer }) => {
 		} else {
 			setHasMinutes.off();
 		}
-
-		setTime(getStopwatchLabel({ ...timer }));
 	}, [timer]);
 
 	useEffect(() => handleCheck(), [timer]);
 
 	return (
 		<TimeProgress color={color} min={0} max={60} value={timer.seconds}>
-			<Text
-				align='center'
-				color={getColor({ theme, colorMode, type: 'text.secondary' })}
-				fontSize={hasHours && hasMinutes ? '4xl' : hasMinutes ? '5xl' : '6xl'}
-				fontFamily='mono'
-				whiteSpace='nowrap'
-				userSelect='none'
-			>
-				{time}
-			</Text>
+			<TimeLabel timer={{ ...timer }} options={{ hours: hasHours, minutes: hasMinutes }} />
 		</TimeProgress>
 	);
 };
