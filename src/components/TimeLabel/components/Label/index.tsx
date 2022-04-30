18ff9154379b5ea03d9224383be0c15f9f4c6758
David Scicluna <dave_nostra@hotmail.com>
2022-04-30 18:32:48 +0200

    Feature: TimeLabel - Label

---
+++ src/components/TimeLabel/components/Label/index.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+
+import { useTheme, getColor } from '@davidscicluna/component-library';
+
+import { useColorMode, VStack, Text } from '@chakra-ui/react';
+
+import { LabelProps } from './types';
+
+const Label: FC<LabelProps> = (props) => {
+	const theme = useTheme();
+	const { colorMode } = useColorMode();
+
+	const { caption, captionSize = 'xs', value, valueSize = '4xl' } = props;
+
+	return (
+		<VStack align='center' justify='center' spacing={0}>
+			<Text
+				align='center'
+				color={getColor({ theme, colorMode, type: 'text.secondary' })}
+				fontSize={captionSize}
+				fontWeight='medium'
+				textTransform='uppercase'
+				whiteSpace='nowrap'
+				userSelect='none'
+				visibility={caption ? 'visible' : 'hidden'}
+			>
+				{caption || ':'}
+			</Text>
+
+			<Text
+				align='center'
+				color={getColor({ theme, colorMode, type: 'text.primary' })}
+				fontSize={valueSize}
+				fontFamily='mono'
+				userSelect='none'
+			>
+				{typeof value === 'number' ? String(value).padStart(2, '0') : value}
+			</Text>
+		</VStack>
+	);
+};
+
+export default Label;
---
+++ src/components/TimeLabel/components/Label/types.ts
@@ -0,0 +1,8 @@
+import { FontSize } from '@davidscicluna/component-library';
+
+export type LabelProps = {
+	caption: string;
+	captionSize?: FontSize;
+	value: string | number;
+	valueSize?: FontSize;
+};
