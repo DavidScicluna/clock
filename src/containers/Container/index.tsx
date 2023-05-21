a193d8e9b4cca14cce6aada045ed5fc80e73c173
DavidScicluna <dave_nostra@hotmail.com>
2023-05-21 21:13:45 +0200

    Fix: Container

--- src/containers/Container/index.tsx
+++ src/containers/Container/index.tsx
@@ -1,36 +1,16 @@
 import { FC } from 'react';
 
-import { DSCLProvider } from '@davidscicluna/component-library';
-
-// import { Provider } from 'react-redux';
-// import { persistStore } from 'redux-persist';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import store from '../../store';
 import Layout from '../Layout';
 import Router from '../Router';
-
-// const persistor = persistStore(store);
+import Routes from '../Routes';
 
 const Container: FC = () => {
-	// const dispatch = useDispatch();
-	// const isSplashscreenOpen = useSelector((state) => state.modals.ui.isSplashscreenOpen);
-
-	// useEffect(() => {
-	// 	if (isSplashscreenOpen) {
-	// 		setTimeout(() => dispatch(toggleSplashscreen(false)), 2500);
-	// 	}
-	// }, [isSplashscreenOpen]);
-
 	return (
-		// <Provider store={store}>
-		// 	<PersistGate loading={null} persistor={persistor}>
-		<DSCLProvider>
-			<Router>
-				<Layout />
-			</Router>
-		</DSCLProvider>
-		// 	</PersistGate>
-		// </Provider>
+		<Router>
+			<Layout>
+				<Routes />
+			</Layout>
+		</Router>
 	);
 };
 
