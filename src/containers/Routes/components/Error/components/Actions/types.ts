fecb6693141350bf0a66f34cceefb4be665013c2
DavidScicluna <dave_nostra@hotmail.com>
2023-05-21 21:17:54 +0200

    Fix: Routes - Removed Error

--- src/containers/Routes/components/Error/components/Actions/types.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonProps } from '@davidscicluna/component-library';
-
-export type ActionsProps = {
-	color: ButtonProps['color'];
-	colorMode: ButtonProps['colorMode'];
-	size: ButtonProps['size'];
-};
