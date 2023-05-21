2a650c4111f7739fafef8f7ff7a869130dad907e
DavidScicluna <dave_nostra@hotmail.com>
2023-05-21 21:21:33 +0200

    Fix: Layout

---
+++ src/containers/Layout/common/types/index.ts
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export type LayoutDeviceType = 'mobile' | 'tablet' | 'desktop';
+
+export type LayoutProps = { children: ReactNode };
--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,96 +1,38 @@
-import { FC, useCallback, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { FC } from 'react';
 
-import { Icon, Space, TabBar, useDebounce, useTheme, utils } from '@davidscicluna/component-library';
+import { useDSCLProviderContext, useTheme, utils } from '@davidscicluna/component-library';
 
-import { Center, VStack } from '@chakra-ui/react';
+import { Container, useMediaQuery } from '@chakra-ui/react';
 
-import { useEffectOnce, useElementSize } from 'usehooks-ts';
+import { LayoutProps } from './common/types';
+import Structure from './components/Structure';
 
-import Routes from '../Routes';
+const { getColor } = utils;
 
-const { convertREMToPixels, convertStringToNumber } = utils;
-
-const defaultActiveTab = 0;
-const spacing: Space = 2;
-
-const Layout: FC = () => {
+const Layout: FC<LayoutProps> = ({ children }) => {
 	const theme = useTheme();
 
-	const location = useLocation();
-	const navigate = useNavigate();
+	const { colorMode } = useDSCLProviderContext();
 
-	const [tabBarRef, { height: tabBarHeight }] = useElementSize();
-
-	const [activeTab, setActiveTab] = useState<number>(defaultActiveTab);
-	const debouncedActiveTab = useDebounce<number>(activeTab);
-
-	const handleRoutesHeight = useCallback((): string => {
-		const spacingHeight = convertREMToPixels(convertStringToNumber(theme.space[spacing], 'rem')) || 0;
-
-		return `calc(100vh - ${tabBarHeight + spacingHeight}px)`;
-	}, [theme, spacing, tabBarHeight]);
-
-	const handleCheckLocation = (): void => {
-		let activeTab = defaultActiveTab;
-
-		switch (location.pathname) {
-			case '/alarm':
-				activeTab = 1;
-				break;
-			case '/stopwatch':
-				activeTab = 2;
-				break;
-			case '/timer':
-				activeTab = 3;
-				break;
-			default:
-				activeTab = 0;
-				break;
-		}
-
-		setActiveTab(activeTab);
-	};
-
-	useEffectOnce(() => handleCheckLocation());
+	// const [isSm] = useMediaQuery(`(min-width: ${theme.breakpoints.sm})`);
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints.xl})`);
 
 	return (
-		<VStack width='100%' spacing={spacing}>
-			<Center width='100%' height={handleRoutesHeight()}>
-				<Routes />
-			</Center>
-
-			<Center ref={tabBarRef} width='100%' position='fixed' bottom={0} zIndex={1}>
-				<TabBar
-					color='blue'
-					activeTab={debouncedActiveTab}
-					tabs={[
-						{
-							renderIcon: (props) => <Icon {...props} icon='language' type='filled' />,
-							label: 'World Clock',
-							onClick: () => navigate('/', { replace: true })
-						},
-						{
-							renderIcon: (props) => <Icon {...props} icon='alarm' type='filled' />,
-							label: 'Alarm',
-							onClick: () => navigate('/alarm', { replace: true })
-						},
-						{
-							renderIcon: (props) => <Icon {...props} icon='timer' type='filled' />,
-							label: 'Stopwatch',
-							onClick: () => navigate('/stopwatch', { replace: true })
-						},
-						{
-							renderIcon: (props) => <Icon {...props} icon='hourglass_empty' type='filled' />,
-							label: 'Timer',
-							onClick: () => navigate('/timer', { replace: true })
-						}
-					]}
-					onChange={(index: number) => setActiveTab(index)}
-					// p={3}
-				/>
-			</Center>
-		</VStack>
+		<Container
+			width='100%'
+			maxWidth={theme.sizes.container.xl}
+			minHeight='100vh'
+			centerContent
+			borderWidth='0px'
+			borderLeftWidth={`${isXl ? 2 : 0}px`}
+			borderRightWidth={`${isXl ? 2 : 0}px`}
+			borderStyle='solid'
+			borderColor={getColor({ theme, colorMode, type: 'divider' })}
+			p={0}
+		>
+			<Structure device={isXl ? 'desktop' : isMd ? 'tablet' : 'mobile'}>{children}</Structure>
+		</Container>
 	);
 };
 
