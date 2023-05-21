cc3bdd2aa6a2e4aa4ccfcb98cf6cc25b3c18fc71
DavidScicluna <dave_nostra@hotmail.com>
2023-05-21 21:13:35 +0200

    Feature: Error

---
+++ src/containers/Error/index.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+
+import { Badge, BadgeLabel, useDSCLProviderContext, useTheme, utils } from '@davidscicluna/component-library';
+
+import { Center, Text, VStack } from '@chakra-ui/react';
+
+import { ErrorProps } from './common/types';
+
+const { getColor } = utils;
+
+const Error: FC<ErrorProps> = ({ code, title, subtitle, renderActions }) => {
+	const theme = useTheme();
+
+	const { color, colorMode } = useDSCLProviderContext();
+
+	return (
+		<Center width='100%' height='100%' minHeight='inherit'>
+			<VStack alignItems='center' justifyContent='center' spacing={2}>
+				<Badge color={color} variant='light'>
+					<BadgeLabel textTransform='uppercase'>{`Error ${code}`}</BadgeLabel>
+				</Badge>
+
+				<VStack alignItems='center' justifyContent='center' spacing={0.5}>
+					<Text
+						align='left'
+						color={getColor({ theme, colorMode, type: 'text.primary' })}
+						fontSize='4xl'
+						fontWeight='bold'
+					>
+						{title}
+					</Text>
+					<Text align='left' color={getColor({ theme, colorMode, type: 'text.secondary' })} fontSize='md'>
+						{subtitle}
+					</Text>
+				</VStack>
+
+				{renderActions ? renderActions({ color, colorMode, size: 'md', variant: 'contained' }) : null}
+			</VStack>
+		</Center>
+	);
+};
+
+export default Error;
