f5312afbd57cb5092b191aa339bf5689456fc9a0
DavidScicluna <dave_nostra@hotmail.com>
2023-05-30 22:47:04 +0200

    Feature: TimeLabel - TimeLabelCaption

---
+++ src/components/TimeLabel/components/TimeLabelCaption/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import { useGetColor } from '@davidscicluna/component-library';
+
+import { Text } from '@chakra-ui/react';
+
+import { TimeLabelCaptionProps } from './common/types';
+
+const TimeLabelCaption: FC<TimeLabelCaptionProps> = ({ children, ...rest }) => {
+	const color = useGetColor({ color: 'gray', type: 'text.secondary' });
+
+	return (
+		<Text
+			{...rest}
+			align='center'
+			color={color}
+			fontSize={['md', 'xl']}
+			fontWeight='bold'
+			lineHeight='normal'
+			textTransform='uppercase'
+		>
+			{children}
+		</Text>
+	);
+};
+
+export default TimeLabelCaption;
