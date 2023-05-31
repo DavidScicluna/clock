29ea2fb478610bcbf5e6644bed57962e259bff32
DavidScicluna <dave_nostra@hotmail.com>
2023-05-31 20:34:27 +0200

    Fix: TimeLabel - TimeLabelNumber - Fixed fontSize

--- src/components/TimeLabel/components/TimeLabelNumber/common/types/index.ts
+++ src/components/TimeLabel/components/TimeLabelNumber/common/types/index.ts
@@ -1,3 +1,5 @@
 import { TextProps } from '@chakra-ui/react';
 
-export type TimeLabelNumberProps = TextProps;
+import { TimeLabelProps } from '../../../../common/types';
+
+export type TimeLabelNumberProps = Pick<TimeLabelProps, 'timerTypes'> & TextProps;
--- src/components/TimeLabel/components/TimeLabelNumber/index.tsx
+++ src/components/TimeLabel/components/TimeLabelNumber/index.tsx
@@ -1,16 +1,74 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
-import { useGetColor } from '@davidscicluna/component-library';
+import { FontSize, useGetColor, useTheme } from '@davidscicluna/component-library';
 
-import { Text } from '@chakra-ui/react';
+import { Text, useMediaQuery } from '@chakra-ui/react';
 
 import { TimeLabelNumberProps } from './common/types';
 
-const TimeLabelNumber: FC<TimeLabelNumberProps> = ({ children, ...rest }) => {
+const TimeLabelNumber: FC<TimeLabelNumberProps> = ({ children, timerTypes, ...rest }) => {
+	const theme = useTheme();
+
+	const [isSm] = useMediaQuery(`(min-width: ${theme.breakpoints.sm})`);
+	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
+	const [isLg] = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`);
+
+	const [fontSize, setFontSize] = useState<FontSize>('4xl');
+
 	const color = useGetColor({ color: 'gray', type: 'text.primary' });
 
+	const handleGetFontSize = (): void => {
+		let fontSize: FontSize;
+
+		switch (timerTypes.length) {
+			case 4: {
+				if (isLg) {
+					fontSize = '8xl';
+					break;
+				} else if (isMd) {
+					fontSize = '7xl';
+					break;
+				} else if (isSm) {
+					fontSize = '6xl';
+					break;
+				} else {
+					fontSize = '4xl';
+					break;
+				}
+			}
+			case 3: {
+				if (isMd) {
+					fontSize = '8xl';
+					break;
+				} else if (isSm) {
+					fontSize = '7xl';
+					break;
+				} else {
+					fontSize = '5xl';
+					break;
+				}
+			}
+			default: {
+				if (isMd) {
+					fontSize = '9xl';
+					break;
+				} else if (isSm) {
+					fontSize = '8xl';
+					break;
+				} else {
+					fontSize = '7xl';
+					break;
+				}
+			}
+		}
+
+		setFontSize(fontSize);
+	};
+
+	useEffect(() => handleGetFontSize(), [timerTypes, isSm, isMd, isLg]);
+
 	return (
-		<Text {...rest} align='center' color={color} fontSize={['7xl', '8xl']} fontWeight='semibold'>
+		<Text {...rest} align='center' color={color} fontSize={fontSize} fontWeight='semibold'>
 			{children}
 		</Text>
 	);
