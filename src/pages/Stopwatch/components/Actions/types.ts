a0ab92711f506247cf684841a06bb815db28dd96
DavidScicluna <dave_nostra@hotmail.com>
2023-05-31 20:38:36 +0200

    Fix: Stopwatch - Removed Actions

--- src/pages/Stopwatch/components/Actions/types.ts
+++
@@ -1,9 +0,0 @@
-import { Timer } from '../../../../common/types';
-
-export type ActionsProps = {
-	hasStarted: boolean;
-	timer: Timer;
-	onReset: () => void;
-	onStartPause: () => void;
-	onSetLap: () => void;
-};
