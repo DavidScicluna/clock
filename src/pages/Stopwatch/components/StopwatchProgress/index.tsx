c174f31a1110bee7854267441a4bdb21abf3d096
DavidScicluna <dave_nostra@hotmail.com>
2023-06-01 17:58:27 +0200

    Fix: Stopwatch - StopwatchProgress

--- src/pages/Stopwatch/components/StopwatchProgress/index.tsx
+++ src/pages/Stopwatch/components/StopwatchProgress/index.tsx
@@ -1,29 +1,50 @@
 import { FC, useEffect, useState } from 'react';
 
+import { useGetColor, useGetThemeAppearance } from '@davidscicluna/component-library';
+
 import { compact } from 'lodash';
 
 import { TimerTypesShort } from '../../../../common/types';
 import { checkTimer } from '../../../../common/utils';
 import TimeLabel from '../../../../components/TimeLabel';
+import { spacing } from '../..';
 
 import { StopwatchProgressProps } from './common/types';
 
 const StopwatchProgress: FC<StopwatchProgressProps> = ({ hasStarted, timer }) => {
-	const [types, setTypes] = useState<TimerTypesShort>([]);
+	const { colorMode } = useGetThemeAppearance();
+
+	const [timerTypes, setTimerTypes] = useState<TimerTypesShort>([]);
+
+	const background = useGetColor({ color: 'gray', type: colorMode === 'light' ? 'lighter' : 'darker' });
+	const borderColor = useGetColor({ color: 'gray', type: 'divider' });
 
 	const handleCheck = (): void => {
 		const { hours: hasHours, minutes: hasMinutes } = checkTimer({ ...timer });
 
 		const updatedTypes: TimerTypesShort = compact([hasHours ? 'h' : null, hasMinutes ? 'm' : null, 's', 'ms']);
 
-		if (JSON.stringify(types) !== JSON.stringify(updatedTypes)) {
-			setTypes([...updatedTypes]);
+		if (JSON.stringify(timerTypes) !== JSON.stringify(updatedTypes)) {
+			setTimerTypes([...updatedTypes]);
 		}
 	};
 
 	useEffect(() => handleCheck(), [timer]);
 
-	return <TimeLabel timerTypes={types} timer={{ ...timer }} isLive={hasStarted} />;
+	return (
+		<TimeLabel
+			backgroundColor={background}
+			borderWidth='2px'
+			borderColor={borderColor}
+			borderStyle='solid'
+			borderRadius='base'
+			timerTypes={timerTypes}
+			timer={timer}
+			isLive={hasStarted}
+			spacing={spacing}
+			p={spacing}
+		/>
+	);
 };
 
 export default StopwatchProgress;
