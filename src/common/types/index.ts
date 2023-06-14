96f23e0bdcd5eab2e87a13e5197475d1481ffba6
DavidScicluna <dave_nostra@hotmail.com>
2023-06-14 21:26:34 +0200

    Fix: Types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,4 +1,11 @@
 export type TimerTypeShort = 'h' | 'm' | 's' | 'ms';
+export type TimerTypesShort = TimerTypeShort[];
+
 export type TimerTypeFull = 'hours' | 'minutes' | 'seconds' | 'milliseconds';
+export type TimerTypesFull = TimerTypeFull[];
 
 export type Timer = Record<TimerTypeFull, number>;
+
+export type WeekDayIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export type TimeFormat = '12hr' | '24hr';
