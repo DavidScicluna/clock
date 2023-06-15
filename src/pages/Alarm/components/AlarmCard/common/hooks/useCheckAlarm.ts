31911f2a85d1e0fcebdd1bcd6e139c79b7b0f7ef
DavidScicluna <dave_nostra@hotmail.com>
2023-06-15 22:06:02 +0200

    Fix: Alarm - AlarmCard - useCheckAlarm

--- src/pages/Alarm/components/AlarmCard/common/hooks/useCheckAlarm.ts
+++ src/pages/Alarm/components/AlarmCard/common/hooks/useCheckAlarm.ts
@@ -1,33 +1,27 @@
-import { useDispatch } from 'react-redux';
-
 import { Nullable, useBoolean } from '@davidscicluna/component-library';
 
 import dayjs from 'dayjs';
-import memoize from 'micro-memoize';
 import { useInterval } from 'usehooks-ts';
 
 import { WeekDayIndex } from '../../../../../../common/types';
 import { getWeekDayValue } from '../../../../../../common/utils';
-import { setAlarm } from '../../../../../../store/slices/Alarms';
 import { Alarm } from '../../../../../../store/slices/Alarms/common/types';
 
 const timeout = 1000;
 
-const getIntervalTimeout = memoize(({ repeat = [], isActive }: Alarm): Nullable<number> => {
+const getIntervalTimeout = ({ repeat = [], isActive }: Alarm): Nullable<number> => {
 	if (repeat.length > 0) {
 		const hasRepeat = repeat.some((day) => getWeekDayValue({ day }) === (dayjs(new Date()).day() as WeekDayIndex));
 		return isActive && hasRepeat ? timeout : null;
 	} else {
 		return isActive ? timeout : null;
 	}
-});
+};
 
 const useCheckAlarm = (alarm: Alarm): boolean => {
-	const dispatch = useDispatch();
-
 	const [hasCompleted, setHasCompleted] = useBoolean();
 
-	const { time, repeat = [] } = alarm;
+	const { time } = alarm;
 
 	const handleCheckIsComplete = (): void => {
 		const today = dayjs(new Date());
@@ -37,17 +31,13 @@ const useCheckAlarm = (alarm: Alarm): boolean => {
 		const { h, m } = time;
 
 		if (h === hour && m === minute) {
-			if (repeat.length === 0) {
-				dispatch(setAlarm({ ...alarm, isActive: false }));
-			}
-
 			setHasCompleted.on();
 		} else {
 			setHasCompleted.off();
 		}
 	};
 
-	useInterval(() => handleCheckIsComplete(), getIntervalTimeout(alarm));
+	useInterval(() => (!hasCompleted ? handleCheckIsComplete() : undefined), getIntervalTimeout(alarm));
 
 	return hasCompleted;
 };
