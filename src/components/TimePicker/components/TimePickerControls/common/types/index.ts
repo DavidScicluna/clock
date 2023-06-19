cbc7bfcc6c4d047472285e20c0419f73706a8db7
DavidScicluna <dave_nostra@hotmail.com>
2023-06-19 22:38:46 +0200

    Fix: TimePicker - TimePickerControls

--- src/components/TimePicker/components/TimePickerControls/common/types/index.ts
+++ src/components/TimePicker/components/TimePickerControls/common/types/index.ts
@@ -1,8 +1,8 @@
-import { IconButtonProps } from '@davidscicluna/component-library';
+import { ButtonProps } from '@davidscicluna/component-library';
 
 import { TimerTypeShort } from '../../../../../../common/types';
 
-export type TimePickerControlsProps = Pick<IconButtonProps, 'isDisabled' | 'size'> & {
+export type TimePickerControlsProps = Pick<ButtonProps, 'isDisabled' | 'size'> & {
 	timerType: TimerTypeShort;
 	mode: 'add' | 'subtract';
 	onPick?: (count: number) => void;
--- src/components/TimePicker/components/TimePickerControls/index.tsx
+++ src/components/TimePicker/components/TimePickerControls/index.tsx
@@ -1,16 +1,14 @@
 import { FC } from 'react';
 
 import {
+	Button,
 	ButtonGroup,
 	ButtonGroupItem,
+	ButtonIcon,
 	HoverOverlay,
-	IconButton,
-	IconButtonIcon,
 	Tooltip,
 	useConst
 } from '@davidscicluna/component-library';
-
-import { Center } from '@chakra-ui/react';
 
 import { formatTimerType } from '../../../../common/utils';
 
@@ -20,74 +18,70 @@ const TimePickerControls: FC<TimePickerControlsProps> = ({ timerType, mode, isDi
 	const fullType = useConst(formatTimerType({ timerType, format: 'full' }));
 
 	return (
-		<Center width='100%'>
-			<ButtonGroup isAttached>
-				<ButtonGroupItem index={0} total={2}>
-					<HoverOverlay>
-						{({ isHovering }) => (
-							<Tooltip
+		<ButtonGroup width='100%' isAttached>
+			<ButtonGroupItem index={0} total={2}>
+				<HoverOverlay>
+					{({ isHovering }) => (
+						<Tooltip
+							color='gray'
+							aria-label={
+								mode === 'add' ? `Add 1 ${fullType} (tooltip)` : `Subtract 1 ${fullType} (tooltip)`
+							}
+							label={mode === 'add' ? `Add 1 ${fullType}` : `Subtract 1 ${fullType}`}
+							placement={mode === 'add' ? 'top' : 'bottom'}
+							isOpen={!isDisabled && isHovering}
+							isDisabled={isDisabled}
+						>
+							<Button
+								aria-label={mode === 'add' ? `Add 1 ${fullType}` : `Subtract 1 ${fullType}`}
 								color='gray'
-								aria-label={
-									mode === 'add' ? `Add 1 ${fullType} (tooltip)` : `Subtract 1 ${fullType} (tooltip)`
-								}
-								label={mode === 'add' ? `Add 1 ${fullType}` : `Subtract 1 ${fullType}`}
-								placement={mode === 'add' ? 'top' : 'bottom'}
-								isOpen={!isDisabled && isHovering}
 								isDisabled={isDisabled}
+								isFullWidth
+								onClick={onPick ? () => onPick(1) : undefined}
+								size={size}
+								variant='monochrome'
 							>
-								<IconButton
-									aria-label={mode === 'add' ? `Add 1 ${fullType}` : `Subtract 1 ${fullType}`}
-									color='gray'
-									isDisabled={isDisabled}
-									onClick={onPick ? () => onPick(1) : undefined}
-									size={size}
-									variant='outlined'
-								>
-									<IconButtonIcon
-										icon={mode === 'add' ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
-										category='outlined'
-									/>
-								</IconButton>
-							</Tooltip>
-						)}
-					</HoverOverlay>
-				</ButtonGroupItem>
-				<ButtonGroupItem index={1} total={2}>
-					<HoverOverlay>
-						{({ isHovering }) => (
-							<Tooltip
+								<ButtonIcon
+									icon={mode === 'add' ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}
+									category='outlined'
+								/>
+							</Button>
+						</Tooltip>
+					)}
+				</HoverOverlay>
+			</ButtonGroupItem>
+			<ButtonGroupItem index={1} total={2}>
+				<HoverOverlay>
+					{({ isHovering }) => (
+						<Tooltip
+							color='gray'
+							aria-label={
+								mode === 'add' ? `Add 10 ${fullType}s (tooltip)` : `Subtract 10 ${fullType}s (tooltip)`
+							}
+							label={mode === 'add' ? `Add 10 ${fullType}s` : `Subtract 10 ${fullType}s`}
+							placement={mode === 'add' ? 'top' : 'bottom'}
+							isOpen={!isDisabled && isHovering}
+							isDisabled={isDisabled}
+						>
+							<Button
+								aria-label={mode === 'add' ? `Add 10 ${fullType}s` : `Subtract 10 ${fullType}s`}
 								color='gray'
-								aria-label={
-									mode === 'add'
-										? `Add 10 ${fullType}s (tooltip)`
-										: `Subtract 10 ${fullType}s (tooltip)`
-								}
-								label={mode === 'add' ? `Add 10 ${fullType}s` : `Subtract 10 ${fullType}s`}
-								placement={mode === 'add' ? 'top' : 'bottom'}
-								isOpen={!isDisabled && isHovering}
 								isDisabled={isDisabled}
+								isFullWidth
+								onClick={onPick ? () => onPick(10) : undefined}
+								size={size}
+								variant='monochrome'
 							>
-								<IconButton
-									aria-label={mode === 'add' ? `Add 10 ${fullType}s` : `Subtract 10 ${fullType}s`}
-									color='gray'
-									isDisabled={isDisabled}
-									onClick={onPick ? () => onPick(10) : undefined}
-									size={size}
-									variant='outlined'
-								>
-									<IconButtonIcon
-										icon={
-											mode === 'add' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down'
-										}
-										category='outlined'
-									/>
-								</IconButton>
-							</Tooltip>
-						)}
-					</HoverOverlay>
-				</ButtonGroupItem>
-			</ButtonGroup>
-		</Center>
+								<ButtonIcon
+									icon={mode === 'add' ? 'keyboard_double_arrow_up' : 'keyboard_double_arrow_down'}
+									category='outlined'
+								/>
+							</Button>
+						</Tooltip>
+					)}
+				</HoverOverlay>
+			</ButtonGroupItem>
+		</ButtonGroup>
 	);
 };
 
