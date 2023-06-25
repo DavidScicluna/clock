f91e0e962306b88a013c6728ac9a11f3853cc2b3
DavidScicluna <dave_nostra@hotmail.com>
2023-06-25 18:08:42 +0200

    Fix: Removed Old Timer

--- src/pages/Timer/common/types/index.ts
+++
@@ -1,5 +0,0 @@
-import { Timer as FullTimer } from '../../../../common/types';
-
-export type Status = 'picker' | 'start' | 'pause';
-
-export type Timer = Omit<FullTimer, 'milliseconds'>;
--- src/pages/Timer/components/TimerProgress/common/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { Status } from '../../../../common/types';
-
-export type TimerProgressProps = {
-	status: Status;
-	elapsed: number;
-	total: number;
-};
