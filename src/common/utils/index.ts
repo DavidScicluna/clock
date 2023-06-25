4996ba92eb0cb24b933cdd798514df0d49664135
DavidScicluna <dave_nostra@hotmail.com>
2023-06-25 22:20:42 +0200

    Fix: Utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,120 +1,59 @@
+import { Undefinable } from '@davidscicluna/component-library';
+
 import dayjs from 'dayjs';
 import { compact } from 'lodash';
 import memoize from 'micro-memoize';
 
-import { TimeFormat, Timer, TimerTypeFull, TimerTypeShort, WeekDayIndex } from '../types';
+import { TimeFormat, TimeTypeShort, WeekDayIndex } from '../types';
 
-type CheckTimerReturn = { [key in TimerTypeFull]: boolean };
+type CheckHasTimeTypesProps = Record<TimeTypeShort, number>;
+type CheckHasTimeTypesReturn = Record<TimeTypeShort, boolean>;
 
-export const checkTimer = memoize((timer: Timer): CheckTimerReturn => {
-	const { hours = 0, minutes = 0, seconds = 0, milliseconds = 0 } = timer;
-
-	let hasHours = false;
-	let hasMinutes = false;
-	let hasSeconds = false;
-	let hasMilliseconds = false;
-
-	if (hours > 0) {
-		hasHours = true;
-	} else {
-		hasHours = false;
-	}
-
-	if (minutes > 0 || (hours > 0 && minutes === 0)) {
-		hasMinutes = true;
-	} else {
-		hasMinutes = false;
-	}
-
-	if (seconds > 0 || (hours > 0 && minutes > 0 && seconds === 0)) {
-		hasSeconds = true;
-	} else {
-		hasSeconds = false;
-	}
-
-	if (milliseconds > 0 || (hours > 0 && minutes > 0 && seconds > 0 && milliseconds === 0)) {
-		hasMilliseconds = true;
-	} else {
-		hasMilliseconds = false;
-	}
-
-	return { hours: hasHours, minutes: hasMinutes, seconds: hasSeconds, milliseconds: hasMilliseconds };
+export const checkHasTimeTypes = memoize((time: CheckHasTimeTypesProps): CheckHasTimeTypesReturn => {
+	const { hr = 0, min = 0, sec = 0, ms = 0 } = time;
+	return {
+		hr: hr > 0,
+		min: min > 0 || (hr > 0 && min === 0),
+		sec: sec > 0 || (hr > 0 && min > 0 && sec === 0),
+		ms: ms > 0 || (hr > 0 && min > 0 && sec > 0 && ms === 0)
+	};
 });
 
-type TimerOptions = { [key in TimerTypeFull]: boolean };
+// export const getTimeValue = memoize((props: GetTimeProps): number => {
+// 	return Number(getTimeLabel(props).replaceAll(/:/g, ''));
+// });
 
-export const getLabel = memoize((timer: Partial<Timer>, options?: Partial<TimerOptions>): string => {
-	const { hours = 0, minutes = 0, seconds = 0, milliseconds = 0 } = timer;
-	const {
-		hours: hasHours = true,
-		minutes: hasMinutes = true,
-		seconds: hasSeconds = true,
-		milliseconds: hasMilliseconds = true
-	} = options || {};
+type FormatTimeTypeProps = { timeType: TimeTypeShort; format: 'full' | 'short' | 'initial' };
 
-	let hr: string | undefined;
-	let min: string | undefined;
-	let sec: string | undefined;
-	let ms: string | undefined;
-
-	if (hours > 0 && hasHours) {
-		hr = String(hours).padStart(2, '0');
-	}
-
-	if ((minutes > 0 || (hours > 0 && minutes === 0)) && hasMinutes) {
-		min = String(minutes).padStart(2, '0');
-	}
-
-	if ((seconds > 0 || (hours > 0 && minutes > 0 && seconds === 0)) && hasSeconds) {
-		sec = String(seconds).padStart(2, '0');
-	}
-
-	if ((milliseconds > 0 || (hours > 0 && minutes > 0 && seconds > 0 && milliseconds === 0)) && hasMilliseconds) {
-		if (seconds === 0) {
-			sec = String(seconds).padStart(2, '0');
-		}
-
-		ms = String(milliseconds).padStart(2, '0');
-	}
-
-	return compact([hr, min, sec, ms]).join(':');
-});
-
-export const getTimerValue = memoize((timer: Partial<Timer>, options?: Partial<TimerOptions>): number =>
-	Number(getLabel({ ...timer }, { ...options }).replaceAll(/:/g, ''))
-);
-
-type FormatTimerTypeProps = { timerType: TimerTypeShort; format: 'full' | 'short' };
-
-export const formatTimerType = memoize(({ timerType, format }: FormatTimerTypeProps): string => {
-	switch (timerType) {
+export const formatTimeType = memoize(({ timeType, format }: FormatTimeTypeProps): string => {
+	switch (timeType) {
 		case 'hr':
-			return format === 'full' ? 'Hour' : 'Hr';
+			return format === 'full' ? 'Hour' : format === 'short' ? 'Hr' : 'H';
 		case 'min':
-			return format === 'full' ? 'Minute' : 'Min';
+			return format === 'full' ? 'Minute' : format === 'short' ? 'Min' : 'M';
 		case 'sec':
-			return format === 'full' ? 'Second' : 'Sec';
+			return format === 'full' ? 'Second' : format === 'short' ? 'Sec' : 'S';
 		case 'ms':
 			return format === 'full' ? 'Millisecond' : 'Ms';
 	}
 });
 
-type FormatTimerNumberProps = { time: number; timerType: TimerTypeShort; timeFormat: TimeFormat };
+type FormatTimeProps = { time: number; timeType: TimeTypeShort; timeFormat: TimeFormat };
 
-export const formatTimerNumber = memoize(({ time, timerType, timeFormat }: FormatTimerNumberProps): string => {
+export const formatTime = memoize(({ time, timeType, timeFormat }: FormatTimeProps): string => {
 	const date = dayjs(new Date()).set(
-		timerType === 'hr' ? 'hour' : timerType === 'min' ? 'minute' : timerType === 'sec' ? 'second' : 'millisecond',
+		timeType === 'hr' ? 'hour' : timeType === 'min' ? 'minute' : timeType === 'sec' ? 'second' : 'millisecond',
 		time
 	);
 
-	if (timerType === 'hr') {
+	if (timeType === 'hr') {
 		if (timeFormat === '12hr') {
 			return date.format('hh');
 		} else {
 			return date.format('HH');
 		}
 	} else {
-		switch (timerType) {
+		switch (timeType) {
 			case 'min':
 				return date.format('mm');
 			case 'sec':
@@ -123,6 +62,43 @@ export const formatTimerNumber = memoize(({ time, timerType, timeFormat }: Forma
 				return date.format('SSS');
 		}
 	}
+});
+
+type GetTimeProps = {
+	time: Record<TimeTypeShort, number>;
+	timeFormat: TimeFormat;
+	options?: Record<TimeTypeShort, boolean>;
+};
+
+export const getTimeLabel = memoize(({ time, timeFormat, options }: GetTimeProps): string => {
+	const { hr = 0, min = 0, sec = 0, ms = 0 } = time;
+	const { hr: hasHr = true, min: hasMin = true, sec: hasSec = true, ms: hasMs = true } = options || {};
+
+	let hours: Undefinable<string>;
+	let minutes: Undefinable<string>;
+	let seconds: Undefinable<string>;
+	let milliseconds: Undefinable<string>;
+
+	if (hr > 0 && hasHr) {
+		hours = formatTime({ time: hr, timeType: 'hr', timeFormat });
+	}
+
+	if ((min > 0 || (hr > 0 && min === 0)) && hasMin) {
+		minutes = formatTime({ time: min, timeType: 'min', timeFormat });
+	}
+
+	if ((sec > 0 || (hr > 0 && min > 0 && sec === 0)) && hasSec) {
+		seconds = formatTime({ time: sec, timeType: 'sec', timeFormat });
+	}
+
+	if ((ms > 0 || (hr > 0 && min > 0 && sec > 0 && ms === 0)) && hasMs) {
+		if (sec === 0) {
+			seconds = formatTime({ time: sec, timeType: 'sec', timeFormat });
+		}
+		milliseconds = formatTime({ time: ms, timeType: 'ms', timeFormat });
+	}
+
+	return compact([hours, minutes, seconds, milliseconds]).join(':');
 });
 
 type GetWeekDayLabelProps = { day: WeekDayIndex; format: 'full' | 'short' | 'initial' };
@@ -146,15 +122,13 @@ export const getWeekDayLabel = memoize(({ day, format }: GetWeekDayLabelProps): 
 	}
 });
 
-type GetWeekDayValueProps = { day: WeekDayIndex };
-
 /**
  * This method will convert our local WeekDayIndex value to the JS Date Day format
  * Local Format: 0 - 6 | Mon - Sun
  * JS Format: 0 - 6 | Sun - Sat
  */
 
-export const getWeekDayValue = memoize(({ day }: GetWeekDayValueProps): WeekDayIndex => {
+export const getWeekDayValue = memoize((day: WeekDayIndex): WeekDayIndex => {
 	switch (day) {
 		case 0:
 			return 1;
