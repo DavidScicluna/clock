8c26d75296b9bc66f1e71e27ec7bdf7031b1a940
DavidScicluna <dave_nostra@hotmail.com>
2023-06-25 18:10:51 +0200

    Feature: Timer - CreateTimer - CreateTimerLabel

---
+++ src/pages/Timer/components/CreateTimer/components/CreateTimerLabel/common/types/index.ts
@@ -0,0 +1,5 @@
+import { UseFormReturn } from 'react-hook-form';
+
+import { CreateTimerForm } from '../../../../common/types';
+
+export type CreateTimerLabelProps = UseFormReturn<CreateTimerForm>;
